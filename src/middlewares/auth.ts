09a6ddb97d7cbfceba7fad70dc16944395ac7354
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-07-30 18:16:22 -0300

    feat: add basic auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,29 @@
 import { NextFunction, Request, Response } from "express";
+import { User } from "../models/User";
 
 export const Auth = {
-  private: (req: Request, res: Response, next: NextFunction) => {
-    const allowed = false
+  private: async (req: Request, res: Response, next: NextFunction) => {
+    let allowed = false
+    const authorization = req.headers.authorization
+
+    if (authorization) {
+      const hash: string = authorization.split(' ')[1]
+      const decoded: string = Buffer.from(hash, 'base64').toString()
+      const data: string[] = decoded.split(':')
+
+      if (data.length === 2) {
+        const hasUser = await User.findOne({
+          where: {
+            email: data[0],
+            password: data[1]
+          }
+        })
+
+        if (hasUser) {
+          allowed = true
+        }
+      }
+    }
 
     if (allowed) {
       next()
